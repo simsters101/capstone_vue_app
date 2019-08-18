05326bbc750b5ab863bf743fe216398f49e80ae7
Sim <simchasitzer1@gmail.com>
2019-08-18 12:11:47 -0400

    make so that user can choose a destination for the rank by price

--- src/views/Destinations.vue
+++ src/views/Destinations.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="destinations">
-    <p>
-      {{ id }}
-    </p>
+    <p>{{ origin.name }}</p>
+    <p>Id: {{ id }}</p>
+    <hr>
     <div v-if="sortDestinationsBy === 'cheapness'">
       <div v-for="destination in origin.origin_trip_destinys">
         <p>{{ destination.name }}</p>
@@ -39,32 +39,42 @@ export default {
   },
   created: function() {
     axios.get("/api/destinations").then(response => {
-      this.destinations = response.data;
-    });
 
-    this.destinations.forEach(function(destination) {
-      if (destination.airport_code === this.$route.query.origin) {
-        let i = 0;
-        console.log(i);
-        this.id = destination.id;
-        i += 1;
-      }
-    });
-
-    console.log(this.id);
-
-    axios.get("/api/destinations/" + this.id).then(response => {
       console.log(response.data);
-      this.origin = response.data;
-    });
+      console.log("in destinations get request");
+      this.destinations = response.data;
 
-    if (this.$route.query.moneyToSpend === 'lotsOfMoney') {
-      this.sortDestinationsBy = 'rank';
-    } else if (this.$route.query.moneyToSpend === 'budget') {
-      this.sortDestinationsBy = 'cheapness';
-    } else if (this.$route.query.moneyToSpend === 'mix') {
-      this.sortDestinationsBy = 'mix';
-    }
+      console.log(this.destinations.length);
+
+      this.destinations.forEach((destination) => {
+        if (this.$route.query.origin === destination.airport_code) {
+          let i = 0;
+          console.log(i);
+          this.id = destination.id;
+          i += 1;
+          console.log("in created loop");
+        }
+      });
+
+      // var i;
+      // for (i = 0; i <= this.destinations.length; i++) {
+      //   console.log(i);
+      // }
+
+      axios.get("/api/destinations/" + this.id).then(response => {
+        console.log(response.data);
+        this.origin = response.data;
+      });
+
+      if (this.$route.query.moneyToSpend === 'lotsOfMoney') {
+        this.sortDestinationsBy = 'rank';
+      } else if (this.$route.query.moneyToSpend === 'budget') {
+        this.sortDestinationsBy = 'cheapness';
+      } else if (this.$route.query.moneyToSpend === 'mix') {
+        this.sortDestinationsBy = 'mix';
+      }
+
+    });
   },
   methods: {}
 };
